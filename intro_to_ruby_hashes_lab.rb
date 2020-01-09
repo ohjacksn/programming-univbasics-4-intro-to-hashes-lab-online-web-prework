30e53cf2647724493fd42f3f38c63eed2df5d1a3
Jackson Gray Morgan <jacksonmo@me.com>
2020-01-09 17:35:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,12 +37,5 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash = {
-    apples: 3
-  }
-    if hash[key]
-      hash[key] += 1
-    else
-      update_hash[] += 1
-    end
+  
 end
